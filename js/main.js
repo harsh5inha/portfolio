3376a1ede770dff8d9caf95390469cb1f70b9f01
Harsh Sinha <hsinha@fb.com>
2021-03-19 21:26:04 -0500

    Reduce image size. Increase speed of dynamic on screen typing. Change styling of Section headers.

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function typeWrite() {
     } else if (this.isDeleting && this.txt === '') {
       this.isDeleting = false;
       this.loopNum++;
-      delta = 100;
+      delta = 750;
     }
 
     setTimeout(function() {
